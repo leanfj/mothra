46046e642e318527506f588d3a9a5cf2c25cbd6d
Leandro Ferreira <leanfj@hotmail.com>
2022-11-29 17:13:10 -0300

    Preparando testes de cliente service e controller

--- server/src/clientes/clientes.controller.spec.ts
+++ server/src/clientes/clientes.controller.spec.ts
@@ -2,13 +2,38 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { ClientesController } from './clientes.controller';
 import { ClientesService } from './clientes.service';
 
+import ClienteInMemoryRepository from '../@core/infra/repository/clienteInMemoryRepository';
+import CreateClienteUseCase from '../@core/application/useCases/cliente/createClienteUseCase';
+import ClienteRepository from '../@core/domain/repository/clienteRepository';
+import GetClienteByIDUseCase from '../@core/application/useCases/cliente/getClienteByIDUseCase';
+
 describe('ClientesController', () => {
   let controller: ClientesController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ClientesController],
-      providers: [ClientesService],
+      providers: [
+        ClientesService,
+        {
+          provide: 'ClienteInMemoryRepository',
+          useClass: ClienteInMemoryRepository,
+        },
+        {
+          provide: CreateClienteUseCase,
+          useFactory: (clienteRepository: ClienteRepository) => {
+            return new CreateClienteUseCase(clienteRepository);
+          },
+          inject: ['ClienteInMemoryRepository'],
+        },
+        {
+          provide: GetClienteByIDUseCase,
+          useFactory: (clienteRepository: ClienteRepository) => {
+            return new GetClienteByIDUseCase(clienteRepository);
+          },
+          inject: ['ClienteInMemoryRepository'],
+        }
+      ],
     }).compile();
 
     controller = module.get<ClientesController>(ClientesController);
--- server/src/clientes/clientes.service.spec.ts
+++ server/src/clientes/clientes.service.spec.ts
@@ -1,12 +1,37 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ClientesService } from './clientes.service';
 
+import ClienteInMemoryRepository from '../@core/infra/repository/clienteInMemoryRepository';
+import CreateClienteUseCase from '../@core/application/useCases/cliente/createClienteUseCase';
+import ClienteRepository from '../@core/domain/repository/clienteRepository';
+import GetClienteByIDUseCase from '../@core/application/useCases/cliente/getClienteByIDUseCase';
+
 describe('ClientesService', () => {
   let service: ClientesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ClientesService],
+      providers: [
+        ClientesService,
+        {
+          provide: 'ClienteInMemoryRepository',
+          useClass: ClienteInMemoryRepository,
+        },
+        {
+          provide: CreateClienteUseCase,
+          useFactory: (clienteRepository: ClienteRepository) => {
+            return new CreateClienteUseCase(clienteRepository);
+          },
+          inject: ['ClienteInMemoryRepository'],
+        },
+        {
+          provide: GetClienteByIDUseCase,
+          useFactory: (clienteRepository: ClienteRepository) => {
+            return new GetClienteByIDUseCase(clienteRepository);
+          },
+          inject: ['ClienteInMemoryRepository'],
+        },
+      ],
     }).compile();
 
     service = module.get<ClientesService>(ClientesService);
