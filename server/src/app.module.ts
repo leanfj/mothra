8382c6addea15b1134f13b05b307ef851a3ee0fb
Leandro Ferreira <leanfj@hotmail.com>
2022-12-08 15:27:35 -0300

    Instalação de ERD generator e criado serviço de conexão do prisma

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service'
 import { ClientesModule } from './clientes/clientes.module'
 import { ProfissionalModule } from './profissional/profissional.module'
 import { ServicoModule } from './servico/servico.module'
+import { PrismaService } from './prisma-service/prisma-service.service'
 
 @Module({
   imports: [ClientesModule, ProfissionalModule, ServicoModule],
   controllers: [AppController],
-  providers: [AppService]
+  providers: [AppService, PrismaService]
 })
 export class AppModule {}
--- server/src/main.ts
+++ server/src/main.ts
@@ -2,11 +2,17 @@ import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { NestExpressApplication } from '@nestjs/platform-express'
 import { AppModule } from './app.module'
+import { PrismaService } from './prisma-service/prisma-service.service'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
+  const dbService: PrismaService = app.get(PrismaService)
+
   app.enableCors()
   app.useGlobalPipes(new ValidationPipe())
+
+  dbService.enableShutdownHooks(app)
+
   await app.listen(3000)
 }
 bootstrap()
